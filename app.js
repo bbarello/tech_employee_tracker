e6e037819564094e3e17feca5e9dd6af794fc9b2
bbarello <bryanbarello@gmail.com>
2020-11-02 16:13:01 -0800

    removed source folder

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Adding dependencies
-
 const mysql = require("mysql");
-const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+const inquirer = require('inquirer');
 
-// establishing database connection
-const database = mysql.createConnection({
+
+//  local host connection settings
+const connection = mysql.createConnection({
     host:'localhost',
     port:8080,
     user:'root',
@@ -13,8 +13,33 @@ const database = mysql.createConnection({
     database:'employeeDB'
   })
 
-  // throws a database connection error 
-  database.connect((err) => {
+  // questions prompt
+  connection.connect(async function(err) {
     if (err) throw err;
-    main();
-  })
+    console.log("Oh no something went wrong!");
+  
+    const {start} = await inquirer.prompt({
+      type: "list",
+      message: "What do you want to do?",
+      name: "start",
+      choices: [
+        {
+          name: "Search by artist",
+          value: findByArtist,
+        },
+        {
+          name: "See artists with 10 or more songs",
+          value: findTopArtists,
+        },
+        {
+          name: "View a range of positions",
+          value: findByPosition,
+        },
+        {
+          name: "Search by song title",
+          value: findByTitle,
+        }
+      ]
+    });
+    start();
+  });
